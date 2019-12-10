4c8dc9e9d9a41ea03dd499cad0bcfd9c04197407
Bruno Conde Kind <brunocondekind@hotmail.com>
2019-12-10 11:44:28 -0300

    almost ready for classification

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ echo "If you already ran this script, you don't need to do so again"
 echo "Start the environment by running:  $ pipenv run jupyter-notebook"
 echo "Or start a sandboxed session with: $ pipenv shell"
 
-# setup python 3.6 env
+# setup python 3.8 env
 command -v pipenv &>/dev/null         || exit 1
-pipenv --venv || pipenv --python 3.6  || exit 1
+pipenv --venv || pipenv --python 3.8  || exit 1
 
 # math, statistics, data science
 pipenv install "Cython"               || exit 1
@@ -14,6 +14,7 @@ pipenv install "pandas"               || exit 1
 pipenv install "scikit-learn"         || exit 1
 
 # notebook, plots
+pipenv install "unidecode"            || exit 1
 pipenv install "matplotlib"           || exit 1
 pipenv install "jupyter"              || exit 1
 pipenv install "notebook"             || exit 1
